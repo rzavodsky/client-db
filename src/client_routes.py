b9dc631d6b317a9fb17b9fd5391a8119080d5187
Tracktark <rikoczz@gmail.com>
2021-10-15 18:45:08 +0200

    Changed database operations in client_routes to sqlalchemy

--- src/client_routes.py
+++ src/client_routes.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 from flask import Blueprint, request, abort, make_response
-from psycopg2.errors import UniqueViolation
 from jsonschema import ValidationError
 
-from db import Client, DoesNotExist, IntegrityError
+from db import db, Client
 from schema_validation import ClientValidator, ClientPATCHValidator
 
 clients = Blueprint("clients", __name__)
@@ -11,45 +10,65 @@ clients = Blueprint("clients", __name__)
 @clients.route("")
 def get_all_clients():
     if request.args.get("q"):
-        clients = Client.select().where(Client.name.contains(request.args.get("q")))
+        query = request.args.get("q")
+        clients = Client.query.filter(Client.name.ilike(f"%{query}%"))
     else:
-        clients = Client.select()
-    return {"data": [client.__data__ for client in clients]}
+        clients = Client.query.all()
+    return {"data": [client.as_dict() for client in clients]}
 
 @clients.route("/<int:client_id>")
 def get_single_client(client_id: int):
-    return Client.get_by_id(client_id).__data__
+    client = Client.query.get(client_id)
+    if not client:
+        raise ClientNotFoundException(client_id)
+    return client.as_dict()
 
 @clients.route("", methods = ["POST"])
 def add_client():
     body = request.json
     ClientValidator.validate(body)
-    client = Client.create(**body)
-    return client.__data__
+    client = Client(**body)
+    client.zruseny = 0
+    db.session.add(client)
+    db.session.commit()
+    return client.as_dict()
 
 
 @clients.route("/<int:client_id>", methods = ["PUT"])
 def update_client(client_id: int):
     body = request.json
     ClientValidator.validate(body)
-    client = Client.get_by_id(client_id)
-    client.__data__ = {**client.__data__, **body}
-    client.save()
-    return client.__data__
+    client = Client.query.get(client_id)
+    if not client:
+        raise ClientNotFoundException(client_id)
+
+    for col, value in body.items():
+        setattr(client, col, value)
+
+    db.session.commit()
+    return client.as_dict()
 
 @clients.route("/<int:client_id>", methods = ["PATCH"])
 def partial_update_client(client_id: int):
     body = request.json
     ClientPATCHValidator.validate(body)
-    client = Client.get_by_id(client_id)
-    client.__data__ = {**client.__data__, **body}
-    client.save()
-    return client.__data__
+    client = Client.query.get(client_id)
+    if not client:
+        raise ClientNotFoundException(client_id)
+
+    for col, value in body.items():
+        setattr(client, col, value)
+
+    db.session.commit()
+    return client.as_dict()
 
 @clients.route("/<int:client_id>", methods = ["DELETE"])
 def delete_client(client_id: int):
-    client = Client.get_by_id(client_id)
-    client.delete_instance(recursive=True) # Deletes all related contacts as well
+    client = Client.query.get(client_id)
+    if not client:
+        raise ClientNotFoundException(client_id)
+    db.session.delete(client)
+    db.session.commit()
     return "", 204 # No Content
 
 class ClientNotFoundException(Exception):
